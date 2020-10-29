32ff54d41f95bbd25ae46f52c83ef7ebc7253034
Alex_Kelly <afkelly14@Gmail.com>
2020-10-29 12:43:33 -0400

    Web scraping for pizza from Bing.com search engine, updated Python version to 3.8 to work

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # web-scraping
+
+## Web scraping project
+
+###### Install Beautiful Soup (pip3 install bs4)
+
+###### Must use python3 version
---
+++ main.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+##use requests to get the data
+import requests
+
+##what do you want to search for?
+search = input('Enter search term: ')
+
+##set a get parameter
+params = {"q": search}
+##build a request
+r = requests.get("https://www.bing.com/search", params=params)
+
+##transform the requests response into beautiful soup to be readable
+soup = BeautifulSoup(r.text)
+print(soup.prettify())
+

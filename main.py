cd7d0ea7e75edea0bb24666dc9d141d8fb9826a1
Alex_Kelly <afkelly14@Gmail.com>
2020-10-30 16:39:50 -0400

    added links for the search term

--- main.py
+++ main.py
@@ -13,5 +13,15 @@ r = requests.get("https://www.bing.com/search", params=params)
 
 ##transform the requests response into beautiful soup to be readable
 soup = BeautifulSoup(r.text, "html.parser")
-print(soup.prettify())
+results = soup.find("ol", {"id": "b_results"})
+links = results.findAll("li", {"class": "b_algo"})
+
+##put links in a for statement and print if they have certain attributes
+for item in links:
+    item_text = item.find("a").text
+    item_href = item.find("a").attrs["href"]
+
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)
 
